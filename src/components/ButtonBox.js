19cae2126a6e4bbdb40bc7da9867209bedb37983
Henrik.Ras <henrik.r@live.se>
2022-06-16 14:32:53 +0200

    Finalized - working calculator

---
+++ src/components/ButtonBox.js
@@ -0,0 +1,10 @@
+import "./ButtonBox.css";
+
+export default function ButtonBox(props) {
+
+    return (
+        <div className="buttonBox">
+        {props.children}
+        </div>
+    );
+}
---
+++ src/components/Screen.js
@@ -0,0 +1,10 @@
+import "./Screen.css";
+
+export default function Screen({value}) {
+
+  return (
+    <form className="screen">
+        <label>{value}</label>
+     </form>
+  );
+}
