fc9d94de8a07095d1bfbcd8155292361311b7ed0
Jonatan Westberg <westpest_1@hotmail.com>
2022-06-16 14:56:20 +0200

    kghk

--- src/components/ButtonBox.js
+++ src/components/ButtonBox.js
@@ -1,6 +1,6 @@
-import "./ButtonBox.css";
+import "./ButtonBox.css"
 
-export default function ButtonBox (props) {
+export default function ButtonBox (props){
 
     return <div className="buttonBox">{props.children}</div>
 }
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,7 +1,7 @@
 import "./Wrapper.css";
 
-export default function Wrapper(props) {
-    return (
-        <div className="wrapper">{props.children}</div>
-    )
-}
+export default function Wrapper({children}) {
+
+
+    return <div className="wrapper">{children}</div>;
+};
