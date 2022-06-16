35c0c440277ccdd6211197d0f884f6ea917eaeba
Jonatan Westberg <westpest_1@hotmail.com>
2022-06-16 11:14:19 +0200

    asfas

---
+++ src/Components/Screen.js
@@ -0,0 +1,11 @@
+import "./Screen.css"
+
+export default function Screen({value}) {
+
+
+    return (
+        <form className="screen">
+            <label>100 + 1333 + 12333 = 13766</label>
+        </form>
+    );
+}
